9ce47b607f2492a26ea8cde51d5cb9053f7c096f
Meghan <meghan.woodruff@cune.org>
2018-11-24 22:13:28 -0600

    Add files via upload

---
+++ ANN.R
@@ -0,0 +1,35 @@
+# Get training data
+all_data <- read.csv('alldata.csv')
+
+# Pick out three features and gift amount
+dataset <- all_data[, c(14,15,21,3)]
+
+# Split data for 10 cross validation
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset, SplitRatio = 0.9)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-4] <- scale(training_set[-4])
+test_set[-4] <- scale(test_set[-4])
+
+# Neural Net
+#install.packages('h2o')
+library(h2o)
+h2o.init(nthreads = -1)
+
+NN <- h2o.deeplearning(y = 'GiftAmount', 
+                       training_frame = as.h2o(training_set),
+                       activation = 'Rectifier',
+                       hidden = c(2,2),
+                       epochs = 100,
+                       train_samples_per_iteration = -2)
+
+# Predicting the test set results
+pred <- h2o.predict(NN, newdata = as.h2o(test_set[-4]))
+
+pred <- as.vector(pred)
+
+
